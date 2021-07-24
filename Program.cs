cc0e3221a80c55d99a7bf957d5ac369119534aed
James Bedford <jamesbedford@protonmail.com>
2021-07-24 16:57:25 +1000

    Working solution.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fibonacci(24));
+        }
+
+        public static int Fibonacci(int n)
+        {
+            return (n <= 1 ? n : Fibonacci(n-1) + Fibonacci(n-2));
+        }
+    }
+}
